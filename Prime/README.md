43e0bd70e9433eb74545113958e0c153357120aa
Will Baker <willbaker1988@hotmail.co.uk>
2019-10-30 15:45:20 +0200

    creation of the staging layer

--- Prime/README.md
+++ Prime/README.md
@@ -1,3 +1,10 @@
+# Prime
+playing around with dbt 
+
+# profiles.yml
+The profile.yml configures the environment and database credentials, this can be found in the .dbt root directory.
+
+
 ### dbt models for [your org/project name]
 
 ---
--- Prime/models/stg_weather_forecasts.sql
+++ Prime/models/stg_weather_forecasts.sql
@@ -1,8 +1,8 @@
-{{ config(schema='raw_staging') }}
+{{ config(materialized='table',schema='raw_staging') }}
 
 SELECT 
-    c.value:id::BIGINT AS id AS city_id   
-  , d.value:dt::INT AS  datetime
+    c.value:id::BIGINT AS city_id   
+  , d.value:dt::INT AS  created_datetime
   , d.value:clouds::INT AS clouds
   , d.value:deg::INT AS deg
   , d.value:humidity::INT AS humidity
@@ -11,6 +11,7 @@ SELECT
   , d.value:speed::INT AS speed  
   , d1.value:description::VARCHAR AS description
   , d1.value:main::VARCHAR AS main
+  , CURRENT_TIMESTAMP AS load_datetime
 FROM
   SNOWFLAKE_SAMPLE_DATA.WEATHER.DAILY_14_TOTAL w,
       lateral flatten(input => w.v, recursive => true) c,
@@ -19,5 +20,5 @@ FROM
 WHERE
     w.t::DATE =  CURRENT_DATE - INTERVAL '1 days'
 AND
-    c.value:country::NVARCHAR IS NOT NULL;
+    c.value:country::NVARCHAR IS NOT NULL
     
